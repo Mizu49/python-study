dde8993ecd18662b0a309db596fdf77d85f35485
Mizuki_LAPTOP <fte.mizuki@gmail.com>
2020-12-30 21:27:30 +0900

    とりあえずPythonを使えるようにした

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # python-study
 pythonの勉強
+
+とりあえずPython環境を作ってみた  
+[参考サイト](https://techacademy.jp/magazine/47408)
---
+++ opt/hoge.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+
+rand_array = np.random.rand(3, 3)
+df = pd.DataFrame(data=rand_array, columns=['a', 'b', 'c'])
+print(df)
